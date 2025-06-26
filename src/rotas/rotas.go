2bfd07ec04d79a25b50559c1040246e81acff1c8
André Teixeira <andre-x8@hotmail.com>
2025-06-25 22:04:40 -0300

    adiciona arquivos de rota d elogin

---
+++ src/rotas/rotas.go
@@ -0,0 +1,23 @@
+package rotas
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Rotas struct {
+	URI                string
+	Metodo             string
+	Funcao             func(http.ResponseWriter, *http.Request)
+	RequerAutenticacao bool
+}
+
+func Configurar(router *mux.Router) *mux.Router {
+	rotas := rotasLogin
+	for _, rota := range rotas {
+		router.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+
+	}
+	return router
+}
