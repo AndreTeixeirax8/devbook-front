ed85b1d266da612596aff70e8bdbbd6b8ec98b5c
André Teixeira <andre-x8@hotmail.com>
2025-07-12 10:37:17 -0300

    adiciona funcao de concorrencia

--- src/modelos/Usuario.go
+++ src/modelos/Usuario.go
@@ -1,6 +1,14 @@
 package modelos
 
-import "time"
+import (
+	"devbook-front/src/config"
+	"devbook-front/src/requisicoes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
 
 type Usuario struct {
 	ID          uint64        `json:"id"`
@@ -11,4 +19,137 @@ type Usuario struct {
 	Seguidores  []Usuario     `json:"seguidores"`
 	Seguindo    []Usuario     `json:"seguindo"`
 	Publicacoes []Publicacoes `json:"publicacoes"`
+}
+
+func BuscarUsuarioCompleto(usuarioID uint64, r *http.Request) (Usuario, error) {
+	canalUsuario := make(chan Usuario)
+	canalSeguidores := make(chan []Usuario)
+	canalSeguindo := make(chan []Usuario)
+	canalPublicacoes := make(chan []Publicacoes)
+
+	go BuscarDadosDoUsuario(canalUsuario, usuarioID, r)
+	go BuscarSeguidores(canalSeguidores, usuarioID, r)
+	go BuscarSeguindo(canalSeguindo, usuarioID, r)
+	go BuscarPublicacoes(canalPublicacoes, usuarioID, r)
+
+	var (
+		usuario     Usuario
+		seguidores  []Usuario
+		seguindo    []Usuario
+		publicacoes []Publicacoes
+	)
+
+	for i := 0; i < 4; i++ {
+		select {
+		case usuarioCarregado := <-canalUsuario:
+			if usuarioCarregado.ID == 0 {
+				return Usuario{}, errors.New("Erro ao buscar Usuario")
+			}
+
+			usuario = usuarioCarregado
+
+		case seguidoresCarregados := <-canalSeguidores:
+			if seguidoresCarregados == nil {
+				return Usuario{}, errors.New("Erro ao buscar os seguidores")
+			}
+
+			seguidores = seguidoresCarregados
+
+		case seguindoCarregados := <-canalSeguindo:
+			if seguindoCarregados == nil {
+				return Usuario{}, errors.New("Erro ao buscar quem o usuario está seguindo")
+			}
+
+			seguindo = seguindoCarregados
+
+		case publicacoesCarregadas := <-canalPublicacoes:
+			if publicacoesCarregadas == nil {
+				return Usuario{}, errors.New("Erro ao buscar as publicacoes")
+			}
+
+			publicacoes = publicacoesCarregadas
+
+		}
+	}
+
+	usuario.Seguidores = seguidores
+	usuario.Seguindo = seguindo
+	usuario.Publicacoes = publicacoes
+	return usuario, nil
+
+}
+
+func BuscarDadosDoUsuario(canal chan<- Usuario, usuarioID uint64, r *http.Request) {
+	url := fmt.Sprintf("%s/usuarios/%d", config.APIURL, usuarioID)
+	response, erro := requisicoes.FazerRequisicaoComAuteticacao(r, http.MethodGet, url, nil)
+	if erro != nil {
+		canal <- Usuario{}
+		return
+	}
+	defer response.Body.Close()
+	var usuario Usuario
+	if erro = json.NewDecoder(response.Body).Decode(&usuario); erro != nil {
+		canal <- Usuario{}
+		return
+	}
+
+	canal <- usuario
+}
+
+func BuscarSeguidores(canal chan<- []Usuario, usuarioID uint64, r *http.Request) {
+	url := fmt.Sprintf("%s/usuarios/%d/seguidores", config.APIURL, usuarioID)
+	response, erro := requisicoes.FazerRequisicaoComAuteticacao(r, http.MethodGet, url, nil)
+	if erro != nil {
+		canal <- nil
+		return
+	}
+	defer response.Body.Close()
+
+	var seguidores []Usuario
+	if erro = json.NewDecoder(response.Body).Decode(&seguidores); erro != nil {
+		canal <- nil
+		return
+	}
+
+	canal <- seguidores
+
+}
+
+func BuscarSeguindo(canal chan<- []Usuario, usuarioID uint64, r *http.Request) {
+	url := fmt.Sprintf("%s/usuarios/%d/seguindo", config.APIURL, usuarioID)
+	response, erro := requisicoes.FazerRequisicaoComAuteticacao(r, http.MethodGet, url, nil)
+	if erro != nil {
+		canal <- nil
+		return
+	}
+	defer response.Body.Close()
+
+	var seguindo []Usuario
+	if erro = json.NewDecoder(response.Body).Decode(&seguindo); erro != nil {
+		canal <- nil
+		return
+	}
+
+	canal <- seguindo
+
+}
+
+func BuscarPublicacoes(canal chan<- []Publicacoes, usuarioID uint64, r *http.Request) {
+	url := fmt.Sprintf("%s/usuarios/%d/publicacoes", config.APIURL, usuarioID)
+	response, erro := requisicoes.FazerRequisicaoComAuteticacao(r, http.MethodGet, url, nil)
+	if erro != nil {
+		canal <- nil
+		return
+	}
+	defer response.Body.Close()
+
+	var publicacoes []Publicacoes
+
+	if erro = json.NewDecoder(response.Body).Decode(&publicacoes); erro != nil {
+		canal <- nil
+		return
+	}
+
+	canal <- publicacoes
+
 }
